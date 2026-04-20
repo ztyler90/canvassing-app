539e2885a1d430db2573a3d9e5f244b3cfb25fe5
Zach Tyler <ztyler90@gmail.com>
2026-04-20 09:24:26 -0400

    asd

--- src/screens/SetPassword.jsx
+++ src/screens/SetPassword.jsx
@@ -227,7 +227,7 @@ function Logo() {
       <img
         src="/logo.png"
         alt="KnockIQ"
-        className="h-32 w-auto object-contain"
+        className="h-48 w-auto object-contain"
       />
       <p className="text-gray-400 text-sm mt-2">Welcome to the team</p>
     </div>
